9c9563a3622a7210e4d58718006b2b1ed8ce1e6f
brunomayerc <brunomayerc@gmail.com>
2019-08-04 10:34:29 -0400

    [BC] Completed RESTFul api

--- images/php/app/app/Listing.php
+++ images/php/app/app/Listing.php
@@ -2,10 +2,26 @@
 
 namespace App;
 
+/**
+ * Library used for parsing html dom nodes
+ * 
+ * @see https://github.com/paquettg/php-html-parser
+ */
 use PHPHtmlParser\Dom;
 
 class Listing 
 {      
+
+    /**
+     * Contains the nodes to find the listings in the order they appear on CL
+     */
+    const CL_LISTING_TREE = [ "#sortable-results", ".rows", "li" ];
+    
+    /**
+     * URL Pattern for CL thumbnail images
+     */
+    const CL_THUMBNAIL_ULR = "https://images.craigslist.org/%s_300x300.jpg";
+    
     /**
      * Location identifier
      *
@@ -65,9 +81,96 @@ class Listing
 
         // Retrieves data from CL
         $this->setLocation_rawHTMLDom();
-        
-        // Loop trough the CL, formatting them
-        die(count($this->location_rawHTMLDom->find('#sortable-results')->find(".rows")->find("li")) . "-");
+
+        // Loop trough all the listings found
+        $listings = [];
+        foreach ( $this->findListings() as $listing ) {
+
+            $listings[] = [
+                "title"         => $this->getNodeInfo( $listing, ".result-title", "text" ),
+                "url"           => $this->getNodeInfo( $listing, ".result-title", "href" ),
+                "bedrooms"      => $this->getNodeInfo( $listing, ".housing", "text" ),
+                "cost"          => $this->getNodeInfo( $listing, ".result-price", "text" ),
+                "location"      => $this->getNodeInfo( $listing, ".result-hood", "text" ),
+                "thumbnails"    => $this->formatThumbnails( $this->getNodeInfo( $listing, ".result-image", "outerHtml" ) )
+            ];
+
+        }
+
+        return $listings;
+
+    }
+    
+
+    /**
+     * Giving a node, finds the images information based on CL pattern
+     *
+     * @param mixed $node Node containing the images info
+     * @return array Thumbnails urls
+     */
+    private function formatThumbnails( $node ) {
+
+        $startDelimiter = "1:";
+        $endDelimiter = ",";
+        $contents = array();
+        $startDelimiterLength = strlen( $startDelimiter );
+        $endDelimiterLength = strlen( $endDelimiter );
+        $startFrom = $contentStart = $contentEnd = 0;
+        while ( false !== ( $contentStart = strpos( $node, $startDelimiter, $startFrom ) ) ) {
+            $contentStart += $startDelimiterLength;
+            $contentEnd = strpos( $node, $endDelimiter, $contentStart );
+            if ( false === $contentEnd ) break;
+            $contents[] = sprintf( self::CL_THUMBNAIL_ULR, substr($node, $contentStart, $contentEnd - $contentStart) );
+            $startFrom = $contentEnd + $endDelimiterLength;
+        }
+
+        return $contents;
+    }
+
+    /**
+     * Giving a dom node, retrieves information from it
+     *
+     * @param mixed $node The DOM node
+     * @param string $selector The selector within that node where the info is located
+     * @param string $attr What attr from that node is being retrieved
+     * @return string|bool Node info if found or false if not found
+     */
+    private function getNodeInfo( $node, $selector, $attr ){
+
+        $nodeInfo = false;
+        $info = $node->find( $selector );
+        if ( count( $info ) > 0 ) {
+
+            $nodeInfo = trim( $info[0]->{$attr} );
+
+        }
+
+        return $nodeInfo;
+
+    }
+    
+    /**
+     * Find and retrieves all the listing nodes within CL
+     *
+     * @return array listing nodes found on CL
+     */
+    private function findListings(){
+
+        $listing_nodes = $this->location_rawHTMLDom;
+        foreach ( self::CL_LISTING_TREE as $selector ) {
+            
+            // Check if the node exists
+            if ( empty( $listing_nodes ) ) {
+
+                throw new \Exception("Craigslist listing node tree is invalid: " . $selector);
+
+            }
+
+            $listing_nodes = $listing_nodes->find( $selector );
+
+        }
+
+        return $listing_nodes;
 
     }
 
@@ -86,7 +189,7 @@ class Listing
             
         } catch ( \Exception $e ) {
             
-            throw new \Exception("Error retrieving data from Craigslist. Please try again.");
+            throw new \Exception("Error retrieving data from Craigslist. " . $e->getMessage() );
             
         }
 
